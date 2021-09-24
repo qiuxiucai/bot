a67d37ed43c89e99e45be7049674754c9650944e
qiuxiucai <69458598+qiuxiucai@users.noreply.github.com>
2021-09-25 01:27:54 +0800

    Update __init__.py

--- jbot/__init__.py
+++ jbot/__init__.py
@@ -3,71 +3,71 @@ import json
 import os
 import logging
 
-JD_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-CONFIG_DIR = f'{JD_DIR}/config'
-SCRIPTS_DIR = f'{JD_DIR}/scripts'
-OWN_DIR = f'{JD_DIR}/own'
-BOT_DIR = f'{JD_DIR}/jbot'
-_DiyScripts = f'{JD_DIR}/diyscripts'
-LOG_DIR = f'{JD_DIR}/log'
-SHORTCUT_FILE = f'{CONFIG_DIR}/shortcut.list'
-BOT_LOG_FILE = f'{LOG_DIR}/bot/run.log'
-BOT_JSON_FILE = f'{CONFIG_DIR}/bot.json'
-QR_IMG_FILE = f'{CONFIG_DIR}/qr.jpg'
-BOT_SET_JSON_FILE_USER = f'{CONFIG_DIR}/botset.json'
-BOT_SET_JSON_FILE = f'{BOT_DIR}/set.json'
+_JdDir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+_ConfigDir = f'{_JdDir}/config'
+_ScriptsDir = f'{_JdDir}/scripts'
+_OwnDir = f'{_JdDir}/own'
+_JdbotDir = f'{_JdDir}/jbot'
+_DiyScripts = f'{_JdDir}/diyscripts'
+_LogDir = f'{_JdDir}/log'
+_shortcut = f'{_ConfigDir}/shortcut.list'
+_botlog = f'{_LogDir}/bot/run.log'
+_botjson = f'{_ConfigDir}/bot.json'
+img_file = f'{_ConfigDir}/qr.jpg'
+_botset = f'{_ConfigDir}/botset.json'
+_set = f'{_JdbotDir}/set.json'
 
-if not os.path.exists(f'{LOG_DIR}/bot'):
-    os.mkdir(f'{LOG_DIR}/bot')
+if not os.path.exists(f'{_LogDir}/bot'):
+    os.mkdir(f'{_LogDir}/bot')
 logging.basicConfig(
-    format='%(asctime)s-%(name)s-%(levelname)s=> [%(funcName)s] %(message)s ', level=logging.INFO, filename=BOT_LOG_FILE,
+    format='%(asctime)s-%(name)s-%(levelname)s=> [%(funcName)s] %(message)s ', level=logging.INFO, filename=_botlog,
     filemode='w')
 logger = logging.getLogger(__name__)
-if os.path.exists(BOT_JSON_FILE):
-    with open(BOT_JSON_FILE, 'r', encoding='utf-8') as f:
-        BOT = json.load(f)
-if os.path.exists(BOT_SET_JSON_FILE_USER):
-    with open(BOT_SET_JSON_FILE_USER, 'r', encoding='utf-8') as f:
-        BOT_SET = json.load(f)
+if os.path.exists(_botjson):
+    with open(_botjson, 'r', encoding='utf-8') as f:
+        bot = json.load(f)
+if os.path.exists(_botset):
+    with open(_botset, 'r', encoding='utf-8') as f:
+        mybot = json.load(f)
 else:
-    with open(BOT_SET_JSON_FILE, 'r', encoding='utf-8') as f:
-        BOT_SET = json.load(f)
-if BOT_SET.get('开启别名') and BOT_SET['开启别名'].lower() == 'true':
-    ch_name = True
+    with open(_set, 'r', encoding='utf-8') as f:
+        mybot = json.load(f)
+if '开启别名' in mybot.keys() and mybot['开启别名'].lower() == 'true':
+    chname = True
 else:
-    ch_name = False
-chat_id = int(BOT['user_id'])
+    chname = False
+chat_id = int(bot['user_id'])
 # 机器人 TOKEN
-TOKEN = BOT['bot_token']
+TOKEN = bot['bot_token']
 # HOSTAPI = bot['apihost']
 # 发消息的TG代理
 # my.telegram.org申请到的api_id,api_hash
-API_ID = BOT['api_id']
-API_HASH = BOT['api_hash']
-PROXY_START = BOT['proxy']
-START_CMD = BOT['StartCMD']
-PROXY_TYPE = BOT['proxy_type']
-connectionType = connection.ConnectionTcpMTProxyRandomizedIntermediate if PROXY_TYPE == "MTProxy" else connection.ConnectionTcpFull
-if BOT.get('proxy_user') and BOT['proxy_user'] != "代理的username,有则填写，无则不用动":
+api_id = bot['api_id']
+api_hash = bot['api_hash']
+proxystart = bot['proxy']
+StartCMD = bot['StartCMD']
+proxyType = bot['proxy_type']
+connectionType = connection.ConnectionTcpMTProxyRandomizedIntermediate if proxyType == "MTProxy" else connection.ConnectionTcpFull
+if 'proxy_user' in bot.keys() and bot['proxy_user'] != "代理的username,有则填写，无则不用动":
     proxy = {
-        'proxy_type': BOT['proxy_type'],
-        'addr':  BOT['proxy_add'],
-        'port': BOT['proxy_port'],
-        'username': BOT['proxy_user'],
-        'password': BOT['proxy_password']}
-elif PROXY_TYPE == "MTProxy":
-    proxy = (BOT['proxy_add'], BOT['proxy_port'], BOT['proxy_secret'])
+        'proxy_type': bot['proxy_type'],
+        'addr':  bot['proxy_add'],
+        'port': bot['proxy_port'],
+        'username': bot['proxy_user'],
+        'password': bot['proxy_password']}
+elif proxyType == "MTProxy":
+    proxy = (bot['proxy_add'], bot['proxy_port'], bot['proxy_secret'])
 else:
-    proxy = (BOT['proxy_type'], BOT['proxy_add'], BOT['proxy_port'])
+    proxy = (bot['proxy_type'], bot['proxy_add'], bot['proxy_port'])
 # 开启tg对话
-if PROXY_START and BOT.get('noretry') and BOT['noretry']:
-    jdbot = TelegramClient('bot', API_ID, API_HASH, connection=connectionType,
+if proxystart and 'noretry' in bot.keys() and bot['noretry']:
+    jdbot = TelegramClient('bot', api_id, api_hash, connection=connectionType,
                            proxy=proxy).start(bot_token=TOKEN)
-elif PROXY_START:
-    jdbot = TelegramClient('bot', API_ID, API_HASH, connection=connectionType,
+elif proxystart:
+    jdbot = TelegramClient('bot', api_id, api_hash, connection=connectionType,
                            proxy=proxy, connection_retries=None).start(bot_token=TOKEN)
-elif BOT.get('noretry') and BOT['noretry']:
-    jdbot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=TOKEN)
+elif 'noretry' in bot.keys() and bot['noretry']:
+    jdbot = TelegramClient('bot', api_id, api_hash).start(bot_token=TOKEN)
 else:
-    jdbot = TelegramClient('bot', API_ID, API_HASH,
+    jdbot = TelegramClient('bot', api_id, api_hash,
                            connection_retries=None).start(bot_token=TOKEN)
